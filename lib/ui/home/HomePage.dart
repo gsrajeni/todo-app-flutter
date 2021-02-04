e6b04ccc5d0e03187826c405b43edf6e2e7a5605
gourango_sutradhar <gourango.sutradhar@sslwireless.com>
2021-02-04 18:18:03 +0600

    updated

--- lib/ui/home/HomePage.dart
+++ lib/ui/home/HomePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo/core/database/MyDatabase.dart';
 import 'package:todo/core/style/color.dart';
+import 'package:todo/core/style/styles.dart';
 import 'package:todo/model/category/CategoryDao.dart';
 import 'package:todo/model/todo/TodoDao.dart';
 import 'package:todo/model/todo/TodoDataModel.dart';
@@ -12,6 +13,7 @@ import 'package:intl/intl.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
     show CalendarCarousel;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:badges/badges.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -21,6 +23,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   DateTime _currentDate =
       DateTime.parse(DateFormat("yyyy-MM-dd").format(DateTime.now()));
+  List<TodoDataModel> dataList = [];
 
   @override
   Widget build(BuildContext context) {
@@ -31,8 +34,7 @@ class _HomePageState extends State<HomePage> {
           IconButton(
               icon: Hero(tag: "add new", child: Icon(Icons.add)),
               onPressed: () async {
-                var response =
-                await Navigator.pushNamed(context, "/addNew");
+                var response = await Navigator.pushNamed(context, "/addNew");
                 if (response != null) setState(() {});
               })
         ],
@@ -47,6 +49,7 @@ class _HomePageState extends State<HomePage> {
             toolbarHeight: 0,
             expandedHeight: 360.h,
             flexibleSpace: FlexibleSpaceBar(
+              collapseMode: CollapseMode.parallax,
               background: CalendarCarousel<Event>(
                 onDayPressed: (DateTime date, List<Event> events) {
                   this.setState(() => _currentDate = date);
@@ -54,38 +57,68 @@ class _HomePageState extends State<HomePage> {
                 weekendTextStyle: TextStyle(
                   color: Colors.red,
                 ),
+                onCalendarChanged: (DateTime dateTime){
+                  setState(() {
+                    _currentDate = dateTime;
+                  });
+                },
                 thisMonthDayBorderColor: Colors.grey,
                 weekFormat: false,
+                dayButtonColor: MyColor.white,
                 selectedDateTime: _currentDate,
-                daysHaveCircularBorder: false,
+                daysHaveCircularBorder: true,
+                selectedDayButtonColor: MyColor.blue,
+                todayButtonColor: MyColor.primaryColor,
+                customDayBuilder: (
+                  bool isSelectable,
+                  int index,
+                  bool isSelectedDay,
+                  bool isToday,
+                  bool isPrevMonthDay,
+                  TextStyle textStyle,
+                  bool isNextMonthDay,
+                  bool isThisMonthDay,
+                  DateTime day,
+                ) {
+                  return FutureBuilder(
+                    future: _getTodos(day),
+                    builder:
+                        (context, AsyncSnapshot<List<TodoDataModel>> snapshot) {
+                      return Center(
+                        child: Badge(
+                          alignment: Alignment.bottomLeft,
+                          showBadge: snapshot?.data?.length == 0 ? false : true,
+                          badgeContent: Text("${snapshot?.data?.length?? "..."}", style: MyStyle.captionStyle(MyColor.white),),
+                          child: Text(DateFormat("dd").format(day)),
+                        ),
+                      );
+                    },
+                  );
+                },
               ),
             ),
           ),
           SliverList(
               delegate: SliverChildListDelegate([
-            Card(
-              elevation: 2,
-              child: FutureBuilder(
-                future: _getTodos(_currentDate),
-                builder: (context, AsyncSnapshot<List<TodoDataModel>> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Container();
+            FutureBuilder(
+              future: _getTodos(_currentDate),
+              builder:
+                  (context, AsyncSnapshot<List<TodoDataModel>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Container();
+                } else {
+                  if (snapshot.data.length == 0) {
+                    return Center(
+                      child: Image.asset(
+                        "assets/images/no_data.png",
+                        fit: BoxFit.fitHeight,
+                      ),
+                    );
                   } else {
-                    if (snapshot.data.length == 0) {
-                      return Expanded(
-                        child: Center(
-                          child: Image.asset(
-                            "assets/images/no_data.png",
-                            fit: BoxFit.fitHeight,
-                          ),
-                        ),
-                      );
-                    } else {
-                      return HomeBody(snapshot.data);
-                    }
+                    return HomeBody(snapshot.data);
                   }
-                },
-              ),
+                }
+              },
             ),
           ])),
         ],
